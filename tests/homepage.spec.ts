d8efbe997260958f19288847f088f284d5047596
pathompat <pathompat@users.noreply.github.com>
2024-12-09 08:15:58 +0000

    deploy: ded2002627312eb33fa5d18c39503928c76ef21a

--- tests/homepage.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.beforeEach(async ({ page }) => {
-  await page.goto('/');
-});
-
-test('test', async ({ page }) => {
-  await expect(page.getByRole('heading', { name: 'รายการสั่งผลิต' })).toBeVisible();
-});
