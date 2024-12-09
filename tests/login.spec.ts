758320ef1991a2452bceabb8739374b33d27c473
pathompat <lw93430@linecorp.com>
2024-12-10 03:01:12 +0700

    add test login and user management

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,31 +6,92 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/login');
 });
 
-test.describe('Failed login', () => {
-  test('should display error when fill uncomplete username', async ({ page }) => {
+test.describe('Validation', () => {
+  test('should display error when fill username 5 symbol', async ({ page }) => {
     await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งาน').click();
-    await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งาน').fill('abc');
-    await page.getByText('บัญชีผู้ใช้งานต้องมีความยาวอย่างน้อย 6 ตัวอักษร').click();
+    await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งาน').fill('abcde');
     await expect(page.locator('#username-messages')).toContainText('บัญชีผู้ใช้งานต้องมีความยาวอย่างน้อย 6 ตัวอักษร');
     const loginBtn = page.getByRole('button', { name: 'เข้าสู่ระบบ' })
     await expect(loginBtn).toBeDisabled()
   });
 
-  test('should display error when fill uncomplete password', async ({ page }) => {
+  test('should display error when fill username with uppercase', async ({ page }) => {
+    await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งาน').click();
+    await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งาน').fill('ABCDEF');
+    await expect(page.locator('#username-messages')).toContainText('บัญชีผู้ใช้งานต้องเป็นตัวอักษรภาษาอังกฤษพิมพ์เล็กเท่านั้น');
+    const loginBtn = page.getByRole('button', { name: 'เข้าสู่ระบบ' })
+    await expect(loginBtn).toBeDisabled()
+  });
+
+  test('should display error when fill username with non-english character', async ({ page }) => {
+    await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งาน').click();
+    await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งาน').fill('หหกดหหฟกฟหหห');
+    await expect(page.locator('#username-messages')).toContainText('บัญชีผู้ใช้งานต้องเป็นตัวอักษรภาษาอังกฤษเท่านั้น');
+    const loginBtn = page.getByRole('button', { name: 'เข้าสู่ระบบ' })
+    await expect(loginBtn).toBeDisabled()
+  });
+
+  test('should display error when fill less than 8 password', async ({ page }) => {
     await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').click();
     await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').fill('asdF1');
     await expect(page.locator('#password-messages')).toContainText('รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษร');
     const loginBtn = page.getByRole('button', { name: 'เข้าสู่ระบบ' })
     await expect(loginBtn).toBeDisabled()
   });
+
+  test('should display error when fill no lowercase password', async ({ page }) => {
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').click();
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').fill('AAASDASDAA');
+    await expect(page.locator('#password-messages')).toContainText('รหัสผ่านต้องมีตัวพิมพ์เล็กอย่างน้อย 1 ตัว');
+    const loginBtn = page.getByRole('button', { name: 'เข้าสู่ระบบ' })
+    await expect(loginBtn).toBeDisabled()
+  });
+
+  test('should display error when fill no uppercase password', async ({ page }) => {
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').click();
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').fill('uasdasdasdsa');
+    await expect(page.locator('#password-messages')).toContainText('รหัสผ่านต้องมีตัวพิมพ์ใหญ่อย่างน้อย 1 ตัว');
+    const loginBtn = page.getByRole('button', { name: 'เข้าสู่ระบบ' })
+    await expect(loginBtn).toBeDisabled()
+  });
+
+  test('should display error when fill no symbol password', async ({ page }) => {
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').click();
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').fill('Uasdasdasdsa');
+    await expect(page.locator('#password-messages')).toContainText('รหัสผ่านต้องมีอักขระพิเศษอย่างน้อย 1 ตัว');
+    const loginBtn = page.getByRole('button', { name: 'เข้าสู่ระบบ' })
+    await expect(loginBtn).toBeDisabled()
+  });
+
+  test('should display error when fill no number password', async ({ page }) => {
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').click();
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').fill('Uasdasdas@');
+    await expect(page.locator('#password-messages')).toContainText('รหัสผ่านต้องมีตัวเลขอย่างน้อย 1 ตัว');
+    const loginBtn = page.getByRole('button', { name: 'เข้าสู่ระบบ' })
+    await expect(loginBtn).toBeDisabled()
+  });
 });
 
-test.describe('Success login', () => {
-  test('login with correct username and password should go to homepage', async ({ page }) => {
+test.describe('Failed', () => {
+  test('should display alert error when wrong username or password', async ({ page }) => {
     await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งานs').click();
-    await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งานs').fill('adminmunggy');
+    await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งานs').fill("testgooduser");
     await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').click();
-    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').fill('Password@1234');
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').fill("G00dP@ssword");
+    await page.getByRole('button', { name: 'เข้าสู่ระบบ' }).click();
+    await expect(page.getByTestId('toast-content')).toBeVisible();
+    await expect(page.getByTestId('toast-content')).toContainText('Invalid username or password');
+  });
+});
+
+test.describe('Success', () => {
+  test('login with correct username and password should go to homepage', async ({ page }) => {
+    const username: string =  process.env.ADMIN_USERNAME || 'admin'
+    const password: string = process.env.ADMIN_PASSWORD || 'Pass@123'
+    await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งานs').click();
+    await page.getByPlaceholder('กรุณากรอกบัญชีผู้ใช้งานs').fill(username);
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').click();
+    await page.getByPlaceholder('กรุณากรอกรหัสผ่าน').fill(password);
     await page.getByRole('button', { name: 'เข้าสู่ระบบ' }).click();
     await page.waitForURL('/');
     await expect(page.getByRole('heading', { name: 'รายการสั่งผลิต' })).toBeVisible();
--- tests/user-management.spec.ts
+++ tests/user-management.spec.ts
@@ -4,11 +4,8 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/user');
 });
 
-test('should see user in nav bar', async ({ page }) => {
+test('should see user menu in nav bar and header', async ({ page }) => {
   await expect(page.getByRole('listbox')).toMatchAriaSnapshot(`- link "จัดการ User"`);
-});
-
-test('should see header', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'จัดการ User' })).toBeVisible();
 });
 
@@ -18,3 +15,66 @@ test('use search non-user in filter input should see no-data', async ({ page }) 
   await filterBox.fill('userinanothersystem331');
   await expect(page.locator('td')).toContainText('No data available');
 });
+
+test.describe('Create User', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.waitForResponse("/api/user/");
+    await page.getByRole('button', { name: /เพิ่มผู้ใช้งาน/i }).click()
+  });
+
+  test('should see input username, password, store name, tier', async ({ page }) => {
+    await expect(page.getByLabel('Username')).toBeVisible();
+    await expect(page.getByLabel('Password')).toBeVisible();
+    await expect(page.getByLabel('ร้านค้า')).toBeVisible();
+  });
+
+  test('should display error when fill username 5 symbol', async ({ page }) => {
+    await page.getByLabel('Username').click();
+    await page.getByLabel('Username').fill('abcde');
+    await expect(page.getByText('บัญชีผู้ใช้งานต้องมีความยาวอย่างน้อย 6 ตัวอักษร')).toBeVisible();
+  });
+
+  test('should display error when fill username with uppercase', async ({ page }) => {
+    await page.getByLabel('Username').click();
+    await page.getByLabel('Username').fill('ABCDEF');
+    await expect(page.getByText('บัญชีผู้ใช้งานต้องเป็นตัวอักษรภาษาอังกฤษพิมพ์เล็กเท่านั้น')).toBeVisible();
+  });
+
+  test('should display error when fill username with non-english character', async ({ page }) => {
+    await page.getByLabel('Username').click();
+    await page.getByLabel('Username').fill('หหกดหหฟกฟหหห');
+    await expect(page.getByText('บัญชีผู้ใช้งานต้องเป็นตัวอักษรภาษาอังกฤษเท่านั้น')).toBeVisible();
+  });
+
+  test('should display error when fill less than 8 password', async ({ page }) => {
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').fill('asdF1');
+    await expect(page.getByText('รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษร')).toBeVisible();
+  });
+
+  test('should display error when fill no lowercase password', async ({ page }) => {
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').fill('AAASDASDAA');
+    await expect(page.getByText('รหัสผ่านต้องมีตัวพิมพ์เล็กอย่างน้อย 1 ตัว')).toBeVisible();
+  });
+
+  test('should display error when fill no uppercase password', async ({ page }) => {
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').fill('uasdasdasdsa');
+    await expect(page.getByText('รหัสผ่านต้องมีตัวพิมพ์ใหญ่อย่างน้อย 1 ตัว')).toBeVisible();
+  });
+
+  test('should display error when fill no symbol password', async ({ page }) => {
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').fill('Uasdasdasdsa');
+    await expect(page.getByText('รหัสผ่านต้องมีอักขระพิเศษอย่างน้อย 1 ตัว')).toBeVisible();
+  });
+
+  test('should display error when fill no number password', async ({ page }) => {
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').fill('Uasdasdas@');
+    await expect(page.getByText('รหัสผ่านต้องมีตัวเลขอย่างน้อย 1 ตัว')).toBeVisible();
+  });
+
+  // TODO: check store name and tier, check can add
+})
