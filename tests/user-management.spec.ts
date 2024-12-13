1ee3d70d63ca260fccf290e15ef976283d188377
pathompat <lw93430@linecorp.com>
2024-12-13 20:57:31 +0700

    add delete test cases & fix edit

--- tests/user-management.spec.ts
+++ tests/user-management.spec.ts
@@ -10,8 +10,20 @@ const randomLowercaseString = (length = 6) =>  {
 }
 
 const randomStr = randomLowercaseString(6)
-const newUser = 'playwright' + randomStr
-const newStore = 'store ' + randomStr
+const NEW_USER = 'playwright' + randomStr
+const NEW_STORE = 'store ' + randomStr
+const USER_TOO_SHORT = 'abcde'
+const USER_ONLY_UPPERCASE = 'ABCDEF'
+const USER_NO_EN_CHAR = 'หหกดหหฟกฟหหห'
+const PASSWORD_TOO_SHORT = 'asdF1'
+const PASSWORD_NO_LOWERCASE = 'AAASDASDAA'
+const PASSWORD_NO_UPPERCASE = 'uasdasdasdsa'
+const PASSWORD_NO_SYMBOL = 'Uasdasdasdsa'
+const PASSWORD_NO_NUMBER = 'Uasdasdas@'
+const PASSWORD_CORRECTLY = 'Password@1234'
+
+let editedUser = ''
+let deletedUser = ''
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/user');
@@ -39,60 +51,70 @@ test.describe('Create User', () => {
     await expect(page.getByLabel('Username')).toBeVisible();
     await expect(page.getByLabel('Password')).toBeVisible();
     await expect(page.getByLabel('ร้านค้า')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill username 5 symbol', async ({ page }) => {
+  test('should display error and disable button when fill username 5 symbol', async ({ page }) => {
     await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill('abcde');
+    await page.getByLabel('Username').fill(USER_TOO_SHORT);
     await expect(page.getByText('บัญชีผู้ใช้งานต้องมีความยาวอย่างน้อย 6 ตัวอักษร')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill username with uppercase', async ({ page }) => {
+  test('should display error and disable button when fill username with uppercase', async ({ page }) => {
     await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill('ABCDEF');
+    await page.getByLabel('Username').fill(USER_ONLY_UPPERCASE);
     await expect(page.getByText('บัญชีผู้ใช้งานต้องเป็นตัวอักษรภาษาอังกฤษพิมพ์เล็กเท่านั้น')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill username with non-english character', async ({ page }) => {
+  test('should display error and disable button when fill username with non-english character', async ({ page }) => {
     await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill('หหกดหหฟกฟหหห');
+    await page.getByLabel('Username').fill(USER_NO_EN_CHAR);
     await expect(page.getByText('บัญชีผู้ใช้งานต้องเป็นตัวอักษรภาษาอังกฤษเท่านั้น')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill less than 8 password', async ({ page }) => {
+  test('should display error and disable button when fill less than 8 password', async ({ page }) => {
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('asdF1');
+    await page.getByLabel('Password').fill(PASSWORD_TOO_SHORT);
     await expect(page.getByText('รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษร')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill no lowercase password', async ({ page }) => {
+  test('should display error and disable button when fill no lowercase password', async ({ page }) => {
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('AAASDASDAA');
+    await page.getByLabel('Password').fill(PASSWORD_NO_LOWERCASE);
     await expect(page.getByText('รหัสผ่านต้องมีตัวพิมพ์เล็กอย่างน้อย 1 ตัว')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill no uppercase password', async ({ page }) => {
+  test('should display error and disable button when fill no uppercase password', async ({ page }) => {
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('uasdasdasdsa');
+    await page.getByLabel('Password').fill(PASSWORD_NO_UPPERCASE);
     await expect(page.getByText('รหัสผ่านต้องมีตัวพิมพ์ใหญ่อย่างน้อย 1 ตัว')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill no symbol password', async ({ page }) => {
+  test('should display error and disable button when fill no symbol password', async ({ page }) => {
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('Uasdasdasdsa');
+    await page.getByLabel('Password').fill(PASSWORD_NO_SYMBOL);
     await expect(page.getByText('รหัสผ่านต้องมีอักขระพิเศษอย่างน้อย 1 ตัว')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill no number password', async ({ page }) => {
+  test('should display error when and disable button fill no number password', async ({ page }) => {
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('Uasdasdas@');
+    await page.getByLabel('Password').fill(PASSWORD_NO_NUMBER);
     await expect(page.getByText('รหัสผ่านต้องมีตัวเลขอย่างน้อย 1 ตัว')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when space on store name', async ({ page }) => {
+  test('should display error and disable button when space on store name', async ({ page }) => {
     await page.getByLabel('ร้านค้า').click();
     await page.getByLabel('ร้านค้า').fill(' ');
     await expect(page.getByText('กรุณากรอกข้อมูล')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
   test('should display correct tier when click', async ({ page }) => {
@@ -121,11 +143,11 @@ test.describe('Create User', () => {
 
   test('should able create user when fill correct information', async ({ page }) => {
     await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill(newUser);
+    await page.getByLabel('Username').fill(NEW_USER);
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('Password#1234');
+    await page.getByLabel('Password').fill(PASSWORD_CORRECTLY);
     await page.getByLabel('ร้านค้า').click();
-    await page.getByLabel('ร้านค้า').fill(newStore);
+    await page.getByLabel('ร้านค้า').fill(NEW_STORE);
 
     const addBtn = await page.getByRole('button', { name: 'บันทึก' })
     await expect(addBtn).toBeEnabled()
@@ -136,10 +158,10 @@ test.describe('Create User', () => {
 
     const filterBox = await page.getByPlaceholder('username,ร้านค้า')
     await expect(filterBox).toBeVisible();
-    await filterBox.fill(newUser);
+    await filterBox.fill(NEW_USER);
 
-    await expect(page.getByRole('cell', { name: newUser })).toBeVisible();
-    await expect(page.getByRole('cell', { name: newStore })).toBeVisible();
+    await expect(page.getByRole('cell', { name: NEW_USER })).toBeVisible();
+    await expect(page.getByRole('cell', { name: NEW_STORE })).toBeVisible();
   });
 
   // TODO: check unable to create duplicate username
@@ -147,39 +169,101 @@ test.describe('Create User', () => {
 
 test.describe('Edit User', () => {
   test.beforeEach(async ({ page }) => {
-    const existingUser = process.env.EXISTING_USERNAME || 'playwrightexisting'
-    const row = await page.locator('tr', { has: page.locator('td', { hasText: existingUser }) });
+    const row = await page.locator('tr', { has: page.locator('td', { hasText: /^playwright/i }) }).first();
+    editedUser = await row.locator('td').nth(1).textContent() || '';
     await row.getByRole('button', { name: /แก้ไข/i }).click()
   });
 
-  test('should display error when fill less than 8 password', async ({ page }) => {
+  test('should display error and disable button when fill less than 8 password', async ({ page }) => {
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('asdF1');
+    await page.getByLabel('Password').fill(PASSWORD_TOO_SHORT);
     await expect(page.getByText('รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษร')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill no lowercase password', async ({ page }) => {
+  test('should display error and disable button when fill no lowercase password', async ({ page }) => {
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('AAASDASDAA');
+    await page.getByLabel('Password').fill(PASSWORD_NO_LOWERCASE);
     await expect(page.getByText('รหัสผ่านต้องมีตัวพิมพ์เล็กอย่างน้อย 1 ตัว')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill no uppercase password', async ({ page }) => {
+  test('should display error and disable button when fill no uppercase password', async ({ page }) => {
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('uasdasdasdsa');
+    await page.getByLabel('Password').fill(PASSWORD_NO_UPPERCASE);
     await expect(page.getByText('รหัสผ่านต้องมีตัวพิมพ์ใหญ่อย่างน้อย 1 ตัว')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill no symbol password', async ({ page }) => {
+  test('should display error and disable button when fill no symbol password', async ({ page }) => {
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('Uasdasdasdsa');
+    await page.getByLabel('Password').fill(PASSWORD_NO_SYMBOL);
     await expect(page.getByText('รหัสผ่านต้องมีอักขระพิเศษอย่างน้อย 1 ตัว')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
   });
 
-  test('should display error when fill no number password', async ({ page }) => {
+  test('should display error and disable button when fill no number password', async ({ page }) => {
     await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('Uasdasdas@');
+    await page.getByLabel('Password').fill(PASSWORD_NO_NUMBER);
     await expect(page.getByText('รหัสผ่านต้องมีตัวเลขอย่างน้อย 1 ตัว')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
+  });
+
+  test('should display error and disable button when space on store name', async ({ page }) => {
+    await page.getByLabel('ร้านค้า').click();
+    await page.getByLabel('ร้านค้า').fill(' ');
+    await expect(page.getByText('กรุณากรอกข้อมูล')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'บันทึก' })).toBeDisabled()
+  });
+
+  test('should able update user when fill correct information', async ({ page }) => {
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').fill(PASSWORD_CORRECTLY);
+    await page.getByLabel('ร้านค้า').click();
+    await page.getByLabel('ร้านค้า').fill(NEW_STORE);
+
+    const addBtn = await page.getByRole('button', { name: 'บันทึก' })
+    await expect(addBtn).toBeEnabled()
+    await addBtn.click();
+
+    const filterBox = await page.getByPlaceholder('username,ร้านค้า')
+    await expect(filterBox).toBeVisible();
+    await filterBox.fill(editedUser);
+
+    await expect(page.getByRole('cell', { name: NEW_STORE })).toBeVisible();
+  });
+
+})
+
+test.describe('Delete User', () => {
+  test.beforeEach(async ({ page }) => {
+    const row = await page.locator('tr', { has: page.locator('td', { hasText: /^playwright/i }) }).first();
+    deletedUser = await row.locator('td').nth(1).textContent() || '';
+    await row.getByRole('button', { name: /ยกเลิก/i }).click()
+  });
+
+  test('should display confirm box before delete', async ({ page }) => {
+    
+    await expect(page.getByRole('dialog')).toContainText('ยืนยันปิดใช้งาน');
+    await expect(page.getByRole('dialog')).toContainText('จะไม่สามารถทำรายการใดๆได้อีก');
+    await expect(page.getByRole('dialog').getByRole('button', { name: 'ยืนยัน' })).toBeVisible();
+    await expect(page.getByRole('dialog').getByRole('button', { name: 'ยกเลิก' })).toBeVisible();
+    await expect(page.locator('.v-overlay__scrim')).toBeVisible();
+  });
+
+  test('should close modal when click cancel button', async ({ page }) => {
+    await page.getByRole('dialog').getByRole('button', { name: 'ยกเลิก' }).click();
+    await expect(page.locator('.v-overlay__scrim')).toBeHidden();
+  });
+
+  test('should remove user in table when click confirm button', async ({ page }) => {
+    await page.getByRole('dialog').getByRole('button', { name: 'ยืนยัน' }).click();
+    await expect(page.getByTestId('toast-content')).toContainText('ยกเลิกใช้งานสำเร็จ');
+    await expect(page.locator('.v-overlay__scrim')).toBeHidden();
+
+    const filterBox = await page.getByPlaceholder('username,ร้านค้า')
+    await filterBox.fill(deletedUser);
+    await expect(page.locator('td')).toContainText('No data available');
   });
 
 })
